e9b1de1a59136fbed59b9b24f7784699c0e5cc78
Mohammed Ismayil <mohammedismayil10@gmail.com>
2022-12-17 22:55:07 +0530

    muverick task home page view has been added

--- Shared/MuverickTask/MuverickTaskHomeView.swift
+++ Shared/MuverickTask/MuverickTaskHomeView.swift
@@ -30,53 +30,86 @@ struct MuverickTaskHomeView: View {
 //        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         NavigationView{
             
-             
-            List {
-                NavigationLink(destination:MuverickCartView(cart: cart)) {
-                    Text("Cart")
-                               }
-                ForEach(products) { item in
-    //                NavigationLink {
-    //
-    //                } label: {
-    //                    Text("Item at")
-    //                }
-                    
-                    
-                   
-                    
-                    HStack{
+//            NavigationLink(destination:MuverickCartView(cart: cart)) {
+//                Text("Cart")
+//            }
+            if #available(iOS 16.0, *) {
+                
+              
+                    List {
                         
-                        AsyncImage(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Ed_Sheeran_%288508826072%29.jpg")) { image in
-                            image
-                                .resizable()
+                        ForEach(products) { item in
+                            //                NavigationLink {
+                            //
+                            //                } label: {
+                            //                    Text("Item at")
+                            //                }
+                            
+                            
+                            
+                            
+                            HStack{
                                 
-                                .cornerRadius(5)
-                                .shadow(radius: 10)
-                                .frame(width: 50, height: 50)
-                            } placeholder: {
-                                Color.white.opacity(1)
-                            }
-                        Text(item.name)
-                        
-                        Text("Add to cart").padding()
-                            .background(.purple)
-                            .cornerRadius(5).onTapGesture {
                                 
-                                if !cart.contains(where: {$0.id == item.id}){
-                                    self.cart.append(item)
+                                
+                                    AsyncImage(url: URL(string: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Ed_Sheeran_%288508826072%29.jpg")) { image in
+                                        image
+                                            .resizable()
+                                        
+                                        //                                    .cornerRadius(5)
+                                        //                                    .shadow(radius: 10)
+                                            .frame(width: 75, height: 100)
+                                    } placeholder: {
+                                        Color.white.opacity(1)
+                                    }
+                               
+                                
+                                VStack(alignment: .leading){
+                                    
+                                    VStack(alignment: .leading){
+                                        Text(item.name)
+                                    }
+                                    
+                                    
+                                    HStack(alignment: .center){
+                                        Text("$25.50")
+                                        Spacer()
+                                        Text("Add cart")
+                                            .foregroundColor(.white)
+                                            .frame(height: 35)
+                                            .padding(.leading)
+                                            .padding(.trailing)
+                                            .background(Color(.blue))
+                                            .cornerRadius(5).onTapGesture {
+                                                
+                                                if !cart.contains(where: {$0.id == item.id}){
+                                                    self.cart.append(item)
+                                                }
+                                                
+                                                
+                                                
+                                                
+                                            }
+                                    }
+                                    
                                 }
                                 
-                                
-                                
-                                
+                                Spacer()
                             }
-                    }.frame(width: UIScreen.main.bounds.width, height: 100, alignment: .top)
-                   
-                }
+//                            .frame(width: UIScreen.main.bounds.width , height: 100, alignment: .center).padding(10).background(Color.gray)
+                            
+                        }.listRowBackground(Color.green)
+                            .listRowInsets( EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) )
+                        
+                    }.listRowSeparator(.hidden).scrollContentBackground(.hidden).background(Color.yellow)
+               
                 
+            }else {
+                // Fallback on earlier versions
             }
         }
+            
+            
 
     }
 }
